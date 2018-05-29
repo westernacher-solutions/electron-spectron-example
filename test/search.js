bfbd8bff6c339070bdb2090bdca30eb324aa0f96
Matthias Bärwolff <matthias.baerwolff@westernacher.com>
2018-05-29 05:22:43 +0200

    simplify test

--- test/search.js
+++ test/search.js
@@ -1,36 +1,35 @@
 const hooks = require('./hooks');
 
 describe('Sample Test', () => {
-    let app;
+  let app;
 
-    beforeEach(() => {
-        return hooks.startApp().then((startedApp) => { app = startedApp });
+  beforeEach(() => {
+    return hooks.startApp().then((startedApp) => {
+      app = startedApp;
     });
+  });
 
-    afterEach(() => {
-        return hooks.stopApp(app)
-    });
+  afterEach(() => {
+    return hooks.stopApp(app);
+  });
 
-    it('opens a window', () => {
-        return app.client.waitUntilWindowLoaded()
-            .getWindowCount().should.eventually.equal(1)
-    });
+  it('opens a window', () => {
+    return app.client.waitUntilWindowLoaded()
+      .getWindowCount().should.eventually.equal(1);
+  });
 
-    it('should get a url', () => {
-        return app.client.url('https://duckduckgo.com')
-            .getTitle()
-            .should.eventually.equal('DuckDuckGo Search Engine')
-    });
+  it('should get a url', () => {
+    return app.client.url('http://google.de')
+      .getTitle()
+      .should.eventually.equal('Google');
+  });
 
-    it('should search', () => {
-        const input = 'this is a test';
-        return app.client.url('https://duckduckgo.com')
-            .setValue('#search_form_input_homepage', input)
-            .getValue("#search_form_input_homepage")
-            .should.eventually.equal(input)
-            .click('#search_button_homepage')
-            .element('.result__a')
-            .should.eventually.exist
-    });
+  it('should accept search input', () => {
+    const input = 'this is a test';
+    return app.client.url('http://google.de')
+      .setValue('input[name=q]', input)
+      .getValue('input[name=q]')
+      .should.eventually.equal(input);
+  });
 
 });
